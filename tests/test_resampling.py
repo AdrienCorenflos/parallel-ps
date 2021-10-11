29f1f523a8fa15868d10a291a00bcadc54fee363
adriencorenflos <adrien.corenflos@gmail.com>
2021-10-11 13:12:05 +0300

    Non trivial test passing

--- tests/test_resampling.py
+++ tests/test_resampling.py
@@ -31,7 +31,7 @@ from parallel_ps.core.resampling import multinomial, systematic, stratified, cou
 
 @pytest.fixture(scope="session", autouse=True)
 def pytest_config():
-    ...
+    jax.config.update("jax_platform_name", "cpu")
 
 
 LIST_RESAMPLINGS = [multinomial, systematic, stratified]
@@ -50,13 +50,13 @@ def _make_inputs(key, T, N, d):
 
 
 @pytest.mark.parametrize("B", [2, 5])
-@pytest.mark.parametrize("N", [1_000, 2_000])
+@pytest.mark.parametrize("N", [50, 100])
 @pytest.mark.parametrize("np_seed", [0, 42])
 @pytest.mark.parametrize("jax_seed", [1234])
 @pytest.mark.parametrize("resampling", LIST_RESAMPLINGS)
 def test_coupled_resampling(B, N, np_seed, jax_seed, resampling):
-    stat_batch_size = 1_000
-    n_samples = N // 2
+    stat_batch_size = 100_000
+    n_samples = 2 * N // 3
     np.random.seed(np_seed)
     jax_key = jax.random.PRNGKey(jax_seed)
     weights = np.random.rand(B, N)
@@ -66,4 +66,6 @@ def test_coupled_resampling(B, N, np_seed, jax_seed, resampling):
     coupled_ancestors = jax.vmap(coupled_resampling, in_axes=[None, 0, None])(weights, batch_keys, n_samples)
 
     count = jax.vmap(lambda z: jnp.bincount(jnp.ravel(z), length=N))(jnp.swapaxes(coupled_ancestors, 0, 1))
-    np.testing.assert_allclose(count / (stat_batch_size * n_samples), weights, atol=1e-3)
+    actual_proba = count / (stat_batch_size * n_samples)
+    np.testing.assert_allclose(np.sum(actual_proba, 1), 1., atol=1e-5)
+    np.testing.assert_allclose(actual_proba, weights, atol=1e-4, rtol=1e-2)
