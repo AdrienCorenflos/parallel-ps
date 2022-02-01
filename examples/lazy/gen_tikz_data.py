9476410e315cd05a5c3e540a7da48a190a022666
Adrien Corenflos <adrien.corenflos@aalto.fi>
2022-02-01 16:11:15 +0200

    Trying to organise the data.

--- examples/lazy/gen_tikz_data.py
+++ examples/lazy/gen_tikz_data.py
@@ -1,7 +1,12 @@
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
-from examples.lazy.rare_events import NS, TS, SVS
+
+# This should be read from the results, but it's easier this way for now.
+SVS = [0.2, 0.3, 0.4, 0.5]
+TS = [32,  64, 128, 256, 512]
+NS = [25, 50, 100, 250, 500, 1_000, 2_500, 5_000]
+
+
 
 lazy_res = np.load(f"./output/rare-events-False-True.npz")
 seq_res = np.load(f"./output/rare-events-True-True.npz")
